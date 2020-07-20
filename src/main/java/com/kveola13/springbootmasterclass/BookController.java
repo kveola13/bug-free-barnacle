0f989f471121c329fb4897832a4aab7b5eae080d
olakv <kveola13@student.westerdals.no>
2020-07-20 11:41:08 +0200

    added a controller

---
+++ src/main/java/com/kveola13/springbootmasterclass/BookController.java
@@ -0,0 +1,17 @@
+package com.kveola13.springbootmasterclass;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class BookController {
+    @GetMapping("/books")
+    public List<Book> getAllBooks() {
+        return Arrays.asList(
+                new Book(1, "The Lord of the Rings", "J.R.R Tolkien"),
+                new Book(2, "The Lord of Chaos", "Robert Jordan"));
+    }
+}
